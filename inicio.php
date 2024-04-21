ee6f73ec435dc19eefd7475de6e992e005aa9d55
Martin Pizano M <mpizanom@gmail.com>
2024-04-21 09:46:39 -0600

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -34,7 +34,7 @@ $pagina_vacia = '';
   }
 </style>
 <div class="page-header">
-  <h2 class="text-center">MODELO EDUCATIVO - CUCEA</h2>
+  <h2 class="text-center">MPM</h2>
 </div>
 <!-- Menú de navegación -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -46,12 +46,6 @@ $pagina_vacia = '';
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
         <a class="nav-link" href="#introduccion">Introducción</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#sobre-nosotros">Sobre nosotros</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#modelo-educativo">Modelo educativo</a>
       </li>
     </ul>
   </div>
@@ -66,76 +60,12 @@ $pagina_vacia = '';
         <h3>Introducción</h3>
       </div>
       <div class="card-body">
-        <p class="text-justify">En la segunda década del siglo XXI, los cambios en la sociedad han sido significativos y han desafiado a las instituciones educativas a adaptarse. Consciente de estos desafíos, la Universidad de Guadalajara, a través del Centro Universitario de Ciencias Económico Administrativas (CUCEA), ha tomado medidas para reestructurar sus funciones académicas y administrativas. Mediante la creación de un modelo educativo, el CUCEA busca responder a las necesidades de una sociedad en constante cambio, en la que los avances tecnológicos y la globalización afectan a todos los aspectos de la vida cotidiana.</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Vjqi_3J8Xxo?si=5xMFJkp6xz6yoJNW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
     </div>
   </section>
 
-  <!-- Sobre nosotros -->
-  <section id="sobre-nosotros" class="my-5">
-    <h2 class="text-center">Sobre nosotros</h2>
-    <!-- Visión y misión -->
-    <div class="card mb-4">
-      <div class="card-header">
-        <h3>Visión y misión</h3>
-      </div>
-      <div class="card-body">
-        <p class="text-justify">La misión del CUCEA es ser un referente académico de excelencia, comprometido con la formación de profesionales capacitados para enfrentar los desafíos del siglo XXI. Su visión es liderar la transformación educativa en el ámbito económico-administrativo, fomentando la innovación, el pensamiento crítico y la responsabilidad social.</p>
-      </div>
-    </div>
-
-    <!-- Historia -->
-    <div class="card mb-4">
-      <div class="card-header">
-        <h3>Historia</h3>
-      </div>
-      <div class="card-body">
-        <p class="text-justify">El CUCEA cuenta con una trayectoria de 30 años en la impartición de programas educativos orientados a las ciencias económico-administrativas. Desde su fundación, ha desempeñado un papel clave en la formación de profesionales exitosos, consolidándose como un referente académico en el ámbito económico-administrativo. El centro se ha adaptado constantemente a los cambios sociales, tecnológicos y económicos, manteniendo su compromiso con la excelencia y la tradición académica.</p>
-      </div>
-    </div>
-  </section>
-
-
-  <!-- Sección Modelo Educativo -->
-  <section id="modelo-educativo" class="my-5">
-    <h2 class="text-center">Modelo Educativo</h2>
-
-    <!-- Descripción -->
-    <div class="card mb-4">
-      <div class="card-header">
-        <h3>Descripción</h3>
-      </div>
-      <div class="card-body">
-        <p class="text-justify">El modelo educativo del CUCEA responde a las demandas de la sociedad actual mediante un enfoque centrado en el estudiante. Promueve la formación continua a lo largo de la vida, la integración de tecnología en el proceso de aprendizaje y la aplicación de la inteligencia distribuida para un enfoque más holístico e inclusivo.</p>
-      </div>
-    </div>
-
-    <!-- Objetivos -->
-    <div class="card mb-4">
-      <div class="card-header">
-        <h3>Objetivos</h3>
-      </div>
-      <div class="card-body">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Formación continua: Oportunidades de aprendizaje continuo.</li>
-          <li class="list-group-item">Integración de tecnología: Uso moderno de tecnología educativa.</li>
-          <li class="list-group-item">Enfoque en inteligencia distribuida: Uso intensivo de redes y tecnología.</li>
-        </ul>
-      </div>
-    </div>
-
-    <!-- Impacto esperado -->
-    <div class="card mb-4">
-      <div class="card-header">
-        <h3>Impacto Esperado</h3>
-      </div>
-      <div class="card-body">
-        <p class="text-justify">El modelo educativo del CUCEA busca tener un impacto positivo tanto en los estudiantes como en la sociedad en general. Al promover una educación centrada en el estudiante y en la tecnología, el CUCEA prepara a los estudiantes para enfrentar los desafíos de un mundo en constante cambio. Además, su compromiso con la formación continua y la inteligencia distribuida ayudará a los estudiantes a convertirse en líderes innovadores y responsables en sus campos.</p>
-        <p class="text-justify">El modelo educativo también contribuye a la sociedad al fomentar la creación de ciudadanos críticos, conscientes y capaces de abordar problemas complejos. En última instancia, el CUCEA aspira a ser un motor de cambio positivo y un ejemplo de excelencia académica en el ámbito económico-administrativo.</p>
-      </div>
-    </div>
-  </section>
-
+ 
   <!-- Botón para abrir datos -->
   <button id="opener" class="btn btn-info btn-lg">Abrir Modelo Educativo</button>
 </div>
